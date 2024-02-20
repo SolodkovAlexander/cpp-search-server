b1a6ea0d059b4f9a19a0f238e92881e9e5941afb
Solodkov Alexander <solodkov.alexander.ru@gmail.com>
2024-02-20 21:47:56 +0300

    Добавлен функционал Paginator для пагинации документов

--- search-server/main.cpp
+++ search-server/main.cpp
@@ -339,14 +339,75 @@ private:
     }
 };
 
-// ==================== для примера =========================
-void PrintDocument(const Document& document) {
+template <typename Iterator>
+class Paginator {
+public:
+    Paginator(Iterator begin_range, Iterator end_range, size_t page_size) {        
+        auto start_page = begin_range;
+        while (start_page < end_range) {
+            auto end_page = start_page + page_size;
+            if (end_page > end_range) {
+                end_page = end_range;
+            };
+            pages_.push_back({start_page, end_page});
+            start_page += page_size;
+        }
+    }
+
+    auto begin() const {
+        return pages_.begin();
+    }
+
+    auto end() const {
+        return pages_.end();
+    }
+
+    auto size() const {
+        return pages_.size();
+    }
+
+private:
+    vector<pair<Iterator, Iterator>> pages_;
+}; 
+
+template <typename Container>
+auto Paginate(const Container& c, size_t page_size) {
+    return Paginator(begin(c), end(c), page_size);
+}
+
+template <typename Iterator>
+ostream& operator<<(ostream& output, const pair<Iterator, Iterator>& page) {
+    for (auto it = page.first; it != page.second; ++it) {
+        cout << *it;
+    }
+    return output;
+} 
+
+ostream& operator<<(ostream& output, const Document& document) {
     cout << "{ "s
          << "document_id = "s << document.id << ", "s
          << "relevance = "s << document.relevance << ", "s
-         << "rating = "s << document.rating << " }"s << endl;
+         << "rating = "s << document.rating << " }"s;
+    return output;
+}
+
+void PrintDocument(const Document& document) {
+    cout << document << endl;
 }
 
 int main() {
-    return 0;
+    SearchServer search_server("and with"s);
+    search_server.AddDocument(1, "funny pet and nasty rat"s, DocumentStatus::ACTUAL, {7, 2, 7});
+    search_server.AddDocument(2, "funny pet with curly hair"s, DocumentStatus::ACTUAL, {1, 2, 3});
+    search_server.AddDocument(3, "big cat nasty hair"s, DocumentStatus::ACTUAL, {1, 2, 8});
+    search_server.AddDocument(4, "big dog cat Vladislav"s, DocumentStatus::ACTUAL, {1, 3, 2});
+    search_server.AddDocument(5, "big dog hamster Borya"s, DocumentStatus::ACTUAL, {1, 1, 1});
+    const auto search_results = search_server.FindTopDocuments("curly dog"s);
+    int page_size = 2;
+    const auto pages = Paginate(search_results, page_size);
+    // Выводим найденные документы по страницам
+    for (auto page = pages.begin(); page != pages.end(); ++page) {
+        cout << *page << endl;
+        cout << "Page break"s << endl;
+    }
 } 
